93049529ec1fa41d06319c705b83c46638141c3c
Erik Sundell <erik@sundellopensource.se>
2022-01-17 00:57:32 +0100

    Att data to work with, tests, and function stub

---
+++ src/main.py
@@ -0,0 +1,14 @@
+def find_common_telephone_numbers():
+    # FIXME: add logic to let this function find the common telephone number
+    #        between the two files
+    #
+    #        1. Learn how to read all lines in a file
+    #        2. Create two sets, each set should hold the lines of the separate files
+    #        3. Return the "intersection" or common elements of the two sets
+    #
+    common_telephone_numbers = {}
+
+    return common_telephone_numbers
+
+
+find_common_telephone_numbers()
